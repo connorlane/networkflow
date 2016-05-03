6dc1c04f01a518263adc9db4c6b0ae828c695c2a
Connor Coward <connor.coward@gmail.com>
2016-05-02 22:17:51 -0500

    Started on the other algorithm

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 #include <cassert>
 #include <queue>
 #include <climits> // INT_MAX
+#include <time.h> // for seeding pseudo-random number generator
+#include <stdlib.h> // srand(), rand()
 
 class Graph {
 private:
@@ -46,33 +48,12 @@ public:
 		return m_sink;
 	}
 
-	std::vector<int>& operator[] (const int index) {
+	const std::vector<int>& operator[] (const int index) const {
 		return m_nodemap[index];
 	};
 
-	std::vector<int> findPath() {
-		std::vector<int> parent(m_size);
-		std::vector<bool> visited(m_size);
-		std::queue<int> q;
-
-		q.push(m_source);
-		visited[m_source] = true;
-		parent[m_source] = -1;
-
-		while(!q.empty()) {
-			int u = q.front();
-			q.pop();
-
-			for (int v = 0; v < m_size; v++) {
-				if (visited[v] == false && m_nodemap[u][v] > 0) {
-					q.push(v);
-					parent[v] = u;
-					visited[v] = true;
-				}
-			}
-		}	
-
-		return (visited[m_sink] == true) ? parent : std::vector<int>();
+	std::vector<int>& operator[] (const int index) {
+		return m_nodemap[index];
 	};
 
 	friend std::ostream& operator<< (std::ostream& os, const Graph& g) {
@@ -98,7 +79,7 @@ public:
 		Graph residual = g;
 		int maxFlow = 0;
 
-		std::vector<int> parent = residual.findPath();
+		std::vector<int> parent = findPath(residual);
 
 		while (!parent.empty()) {
 			v = g.sink();
@@ -119,32 +100,95 @@ public:
 
 			maxFlow += thisFlow;
 
-			parent = residual.findPath();
-
-			std::cout << "parent[]: " << std::endl;
-			for(auto i: parent) {
-				std::cout << i << ' ';
-			}
-			std::cout << std::endl;
+			parent = findPath(residual);
 		}
 
 		return maxFlow;
 	};
+
+private:
+	virtual std::vector<int> findPath(const Graph& g) = 0;
+};
+
+class EdmondsKarpSolver : public FordFulkersonSolver {
+private:
+	std::vector<int> findPath(const Graph& g) {
+		std::vector<int> parent(g.size());
+		std::vector<bool> visited(g.size());
+		std::queue<int> q;
+
+		q.push(g.source());
+		visited[g.source()] = true;
+		parent[g.source()] = -1;
+
+		while(!q.empty()) {
+			int u = q.front();
+			q.pop();
+
+			for (int v = 0; v < g.size(); v++) {
+				if (visited[v] == false && g[u][v] > 0) {
+					q.push(v);
+					parent[v] = u;
+					visited[v] = true;
+				}
+			}
+		}	
+
+		return (visited[g.sink()] == true) ? parent : std::vector<int>();
+	};
+};
+
+class EdmondsKarpSolver : public FordFulkersonSolver {
+private:
+	std::vector<int> findPath(const Graph& g) {
+		std::vector<int> parent(g.size());
+		std::vector<bool> visited(g.size());
+		std::queue<int> q;
+
+		q.push(g.source());
+		visited[g.source()] = true;
+		parent[g.source()] = -1;
+
+		while(!q.empty()) {
+			int u = q.front();
+			q.pop();
+
+			for (int v = 0; v < g.size(); v++) {
+				if (visited[v] == false && g[u][v] > 0) {
+					q.push(v);
+					parent[v] = u;
+					visited[v] = true;
+				}
+			}
+		}	
+
+		return (visited[g.sink()] == true) ? parent : std::vector<int>();
+	};
+
+	std::vector<int> findPath(const Graph& g, int node) {
+		std::vector<int> parent(g.size());
+		std::vector<bool> visited(g.size());
+		
+	}
 };
 
 int main() {
-	Graph g ({
-		{0, 16, 13, 0, 0, 0},
-		{0, 0, 10, 12, 0, 0},
-		{0, 4, 0, 0, 14, 0},
-		{0, 0, 9, 0, 0, 20},
-		{0, 0, 0, 7, 0, 4},
-		{0, 0, 0, 0, 0, 0}
-	}, 0, 5);
+	const int numberOfNodes = 100;
+	const int maxFlowPerLink = 9;
 
-	FordFulkersonSolver s;
+	std::vector< std::vector<int> > nodemap(numberOfNodes, std::vector<int>(numberOfNodes));
 
-	std::cout << "Is empty? " << (std::vector<int>()).empty() << std::endl;
+	srand(time(NULL));
+
+	for (auto& node: nodemap) {
+		for (auto& link: node) {
+			link = rand() % (maxFlowPerLink + 1);
+		}
+	}
+
+	Graph g (nodemap, 0, numberOfNodes - 1);
+
+	EdmondsKarpSolver s;
 
 	std::cout << "Max Flow: " << s.solve(g) << std::endl;
 
