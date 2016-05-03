12f4b686a1a9b722c9beca3ae30a812d510ba23c
Connor Coward <connor.coward@gmail.com>
2016-05-02 19:37:43 -0500

    Finished Ford-Fulkerson algorithm
    Now returns a sane result. Needs testing & asserts

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <cassert>
 #include <queue>
+#include <climits> // INT_MAX
 
 class Graph {
 private:
@@ -33,6 +34,18 @@ public:
 		m_nodemap = nodemap;	
 	};
 
+	int source() const {
+		return m_source;
+	}
+
+	int size() const {
+		return m_size;
+	}
+
+	int sink() const {
+		return m_sink;
+	}
+
 	std::vector<int>& operator[] (const int index) {
 		return m_nodemap[index];
 	};
@@ -40,11 +53,37 @@ public:
 	std::vector<int> findPath() {
 		std::vector<int> parent(m_size);
 		std::vector<bool> visited(m_size);
+		std::queue<int> q;
 
-		
+		q.push(m_source);
+		visited[m_source] = true;
+		parent[m_source] = -1;
 
-		return parent;
+		while(!q.empty()) {
+			int u = q.front();
+			q.pop();
+
+			for (int v = 0; v < m_size; v++) {
+				if (visited[v] == false && m_nodemap[u][v] > 0) {
+					q.push(v);
+					parent[v] = u;
+					visited[v] = true;
+				}
+			}
+		}	
+
+		return (visited[m_sink] == true) ? parent : std::vector<int>();
 	};
+
+	friend std::ostream& operator<< (std::ostream& os, const Graph& g) {
+		for (auto node: g.m_nodemap) {
+			for (auto link: node) {
+				os << link << ' ';
+			}
+			os << std::endl;
+		}
+		return os;
+	}
 };
 
 class MaxFlowSolver {
@@ -55,12 +94,41 @@ public:
 class FordFulkersonSolver : public MaxFlowSolver {
 public:
 	int solve(const Graph& g) {
+		int u, v;
 		Graph residual = g;
-		//int maxFlow = 0;
-		while (!residual.findPath().empty()) {
-			
+		int maxFlow = 0;
+
+		std::vector<int> parent = residual.findPath();
+
+		while (!parent.empty()) {
+			v = g.sink();
+			u = parent[v];
+			int thisFlow = residual[u][v];
+
+			do {
+				thisFlow = (thisFlow < residual[u][v]) ? thisFlow : residual[u][v];
+				v = parent[v];
+				u = parent[v];
+			} while (v != g.source());
+
+			for (v = g.sink(); v != g.source(); v = parent[v]) {
+				u = parent[v];
+				residual[u][v] -= thisFlow;
+				residual[v][u] += thisFlow;
+			}
+
+			maxFlow += thisFlow;
+
+			parent = residual.findPath();
+
+			std::cout << "parent[]: " << std::endl;
+			for(auto i: parent) {
+				std::cout << i << ' ';
+			}
+			std::cout << std::endl;
 		}
-		return residual[0][0];
+
+		return maxFlow;
 	};
 };
 
@@ -73,5 +141,13 @@ int main() {
 		{0, 0, 0, 7, 0, 4},
 		{0, 0, 0, 0, 0, 0}
 	}, 0, 5);
+
+	FordFulkersonSolver s;
+
+	std::cout << "Is empty? " << (std::vector<int>()).empty() << std::endl;
+
+	std::cout << "Max Flow: " << s.solve(g) << std::endl;
+
+	std::cout << g << std::endl;
 }
 
